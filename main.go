4d8998b973f9c5186a4bf7bac51fa07ea4cd0257
Jared Chapmanp <jaredpchapman@gmail.com>
2025-08-07 22:53:57 -0600

    add flags for adjusting configuration

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image/jpeg"
 	"image/png"
@@ -9,9 +10,27 @@ import (
 	"github.com/linuxfreak003/color-reducer/adapters"
 )
 
+var (
+	nColors         int
+	inputFilename   string
+	reducedFilename string
+	outlineFilename string
+	outline         bool
+)
+
+func init() {
+	flag.IntVar(&nColors, "colors", 24, "Number of colors to reduce to")
+	flag.StringVar(&inputFilename, "input", "", "Input file (.jpg)")
+	flag.StringVar(&reducedFilename, "reduced-output", "reduced.png", "Filname of reduced image (png)")
+	flag.StringVar(&outlineFilename, "outline-output", "outline.png", "Filename of outline image (png)")
+	flag.BoolVar(&outline, "outline", false, "Specify whether to include outline image")
+}
+
 func main() {
+	flag.Parse()
+
 	// Load an image (replace with your image loading logic)
-	file, err := os.Open("sunset.jpeg")
+	file, err := os.Open(inputFilename)
 	if err != nil {
 		fmt.Println("Error opening image:", err)
 		return
@@ -25,12 +44,13 @@ func main() {
 	}
 
 	reducer := adapters.NewReducer()
-	colors := reducer.SampleColors(img, 24)
-	newImg := reducer.ReduceImage(img, colors)
-	outlineImg := reducer.OutlineImage(newImg)
+	fmt.Println("Sampling Colors...")
+	colors := reducer.SampleColors(img, nColors)
 
 	// Save the modified image
-	reducedFile, err := os.Create("reduced.png")
+	fmt.Println("Generating reduced image...")
+	newImg := reducer.ReduceImage(img, colors)
+	reducedFile, err := os.Create(reducedFilename)
 	if err != nil {
 		fmt.Println("Error: Could not create output file", err)
 		return
@@ -44,17 +64,21 @@ func main() {
 	}
 
 	// Save the outline image
-	outlineFile, err := os.Create("outline.png")
-	if err != nil {
-		fmt.Println("Error: Could not create outline file", err)
-		return
-	}
-	defer outlineFile.Close()
+	if outline {
+		fmt.Println("Generating Outline...")
+		outlineImg := reducer.OutlineImage(newImg)
+		outlineFile, err := os.Create("outline.png")
+		if err != nil {
+			fmt.Println("Error: Could not create outline file", err)
+			return
+		}
+		defer outlineFile.Close()
 
-	err = png.Encode(outlineFile, outlineImg)
-	if err != nil {
-		fmt.Println("Error: Could not encode outline image", err)
-		return
+		err = png.Encode(outlineFile, outlineImg)
+		if err != nil {
+			fmt.Println("Error: Could not encode outline image", err)
+			return
+		}
 	}
 
 	fmt.Println("Done!")
